d3271310bdb911ac0e59d84ce02bd1f06d7d374d
xeshan <x.eshan@hotmail.com>
2011-07-15 15:43:29 +0500

    sg devise+ profile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studguru::Application.config.secret_token = 'f30fc6fa1f5268b6e3ee74dd2430092f656e796017d735d4a0c55e9cc83d2e5620797587d5b2c285720a25033a7f6f5f74de1e6269291d53a0ab40fba903a4c6'
